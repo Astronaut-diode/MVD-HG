732229c8ddf62232d866d4954abbb9369b23aea7
eegh0407 <925791559@qq.com>
2022-07-02 11:10:13 +0800

    添加set_method_detail方法，可以将项目中所有的节点进行分类，同时为其中的FunctionDefinition节点和ModifierDefinition节点设置method_name还有params

--- read_ast.py
+++ read_ast.py
@@ -50,6 +50,18 @@ def read_ast():
                 content = json.load(ast_json)
                 # 将文件中的内容转化为图结构的数据，传入的内容有抽象语法树的内容，当前项目的所有节点列表，还有读入当前文件之前已经有多少个节点了，这个待会进去会变得，所以先记录下来。
                 create_graph(content, project_node_list, len(project_node_list), data_sol_source_dir_path + project_dir_name + "/" + ast_json_file_name.replace(".json", ".sol"))
+        # 此时整个工程文件夹中的节点都已经构建完成，将这些节点按照节点类型分类，同时放到字典中，保存的格式{"nodeType1": [...], "nodeType2": [...]}
+        project_node_dict = {}
+        # 循环工程文件夹内所有的节点，为了将这些节点分类保存起来。
+        for node in project_node_list:
+            node_type = node.node_type
+            # 如果当前元素的节点类型不存在，先创建对应的key，然后再往里面插值
+            if node_type not in project_node_dict.keys():
+                project_node_dict[node_type] = []
+            # 为对应的属性添加对应的节点。
+            project_node_dict[node_type].append(node)
+        # 设置FunctionDefinition还有ModifierDefinition节点中的method_name还有params两个参数，方便后面设置控制流的时候的操作。
+        set_method_detail(project_node_dict)
         # 循环当前项目中的所有节点，如果节点的类型已经存在了，直接记录下来，否则先创建key然后当作数组往里面添加。
         for node in project_node_list:
             if total_node_list.__contains__(node.node_type):
@@ -161,3 +173,92 @@ def create_graph(content, node_list, node_list_len, source_file_name):
 def create_new_node(node_id, node_type, parent):
     node = Node(node_id, node_type, parent)
     return node
+
+
+# 设置函数的method_name等详细信息到attribute上。
+def set_method_detail(project_node_dict):
+    # 循环所有的FunctionDefinition节点，找出其中的method_name作为新的属性。
+    for node in project_node_dict['FunctionDefinition']:
+        # 为了取出方法的名字，先获取start和end，然后去切分字符串。
+        start = node.attribute['src_code'][0].index(" ") + 1
+        end = node.attribute['src_code'][0].index(")") + 1
+        # 切分字符串，获取函数的完全信息
+        # function call(uint a) public pure returns(uint){
+        # 假如上面这个例子，取出来的是call(uint a)
+        method_full_content = node.attribute['src_code'][0][start: end]
+        # 如果长度是0，或者第一个元素是(，都是说明当前的函数是构造函数
+        if len(method_full_content) == 0 or method_full_content[0] == "(":
+            ancestor = node
+            # 只要还没有到达合约的节点，就一直网上寻找，直到找到合约的名字，作为构造的方法。
+            while not ancestor.node_type == "ContractDefinition":
+                ancestor = ancestor.parent
+                # 找到了合约定义的地方，使用合约的名字作为函数名
+                if ancestor.node_type == "ContractDefinition":
+                    # 这个是函数的名字,保存下来的是ContractName这样子,没有括号也没有参数
+                    method_name = ancestor.attribute['name'][0]
+                    # 被切分以后的参数名字
+                    after_split_params = method_full_content.replace("(", "").replace(")", "").split(",")
+                    # 存放最终的参数的数组，使用数组，里面只保存对应的参数的类型。
+                    params = []
+                    # 这里取到的部分还是uint a这种，还需要再次切分
+                    for param in after_split_params:
+                        # 如果长度是0，直接跳过。
+                        if len(param) == 0:
+                            continue
+                        # 需要考虑到后面的参数会有空格开头的情况，比如function main(uint a, uint b)
+                        if param.split(" ")[0] != "":
+                            params.append(param.split(" ")[0])
+                        else:
+                            params.append(param.split(" ")[1])
+                    # 将这里的函数名字和参数都添加到FunctionDefinition节点的attribute上。
+                    node.append_attribute("method_name", method_name)
+                    node.append_attribute("params", params)
+        # 说明不是构造函数，是普通函数
+        else:
+            # 取出main(uint a, uint b)中的函数名字。
+            method_name = method_full_content[0: method_full_content.index("(")]
+            # 存放最终的参数的数组，使用数组，里面只保存对应的参数的类型。
+            params = []
+            # 被切分以后的参数名字，这里面保存的是['uint a', ' uint b',...]
+            after_split_params = method_full_content.replace(method_name, "").replace("(", "").replace(")", "").split(",")
+            # 这里取到的部分还是uint a这种，还需要再次切分
+            for param in after_split_params:
+                # 如果长度是0，直接跳过。
+                if len(param) == 0:
+                    continue
+                # 需要考虑到后面的参数会有空格开头的情况，比如function main(uint a, uint b)
+                if param.split(" ")[0] != "":
+                    params.append(param.split(" ")[0])
+                else:
+                    params.append(param.split(" ")[1])
+            # 将这里的函数名字和参数都添加到FunctionDefinition节点的attribute上。
+            node.append_attribute("method_name", method_name)
+            node.append_attribute("params", params)
+    # 循环所有的ModifierDefinition节点，找出其中的method_name作为新的属性。
+    for node in project_node_dict['ModifierDefinition']:
+        # 为了取出方法的名字，先获取start和end，然后去切分字符串。
+        start = node.attribute['src_code'][0].index(" ") + 1
+        end = node.attribute['src_code'][0].index(")") + 1
+        # 切分字符串，获取函数的完全信息
+        # modifier onlyOwner(uint a) {
+        # 假如上面这个例子，取出来的是onlyOwner(uint a)
+        method_full_content = node.attribute['src_code'][0][start: end]
+        # 取出onlyOwner(uint a)中的函数名字，onlyOwner
+        method_name = method_full_content[0: method_full_content.index("(")]
+        # 存放最终的参数的数组，使用数组，里面只保存对应的参数的类型。
+        params = []
+        # 被切分以后的参数名字，这里面保存的是['uint a', ' uint b',...]
+        after_split_params = method_full_content.replace(method_name, "").replace("(", "").replace(")", "").split(",")
+        # 这里取到的部分还是uint a这种，还需要再次切分
+        for param in after_split_params:
+            # 如果长度是0，直接跳过。
+            if len(param) == 0:
+                continue
+            # 需要考虑到后面的参数会有空格开头的情况，比如onlyOwner(uint a, uint b)
+            if param.split(" ")[0] != "":
+                params.append(param.split(" ")[0])
+            else:
+                params.append(param.split(" ")[1])
+        # 将这里的函数名字和参数都添加到ModifierDefinition节点的attribute上。
+        node.append_attribute("method_name", method_name)
+        node.append_attribute("params", params)
