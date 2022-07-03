1eeabc0b6fdf317f94a0f0a0d7a3d7a044461144
eegh0407 <925791559@qq.com>
2022-07-03 10:54:11 +0800

    修改了删除部分，可以直接递归的删除内容

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from generate_ast import generate_ast
 from read_ast import read_ast
 from remove_annotation import remove_annotation
 import os
-
+import shutil
 if __name__ == '__main__':
     # 在运行之前先删除之前轮次运行出来的结果
     for project_name in os.listdir("/home/xjj/AST-GNN/data/AST_json/"):
-        os.rmdir("/home/xjj/AST-GNN/data/AST_json/" + project_name)
+        shutil.rmtree("/home/xjj/AST-GNN/data/AST_json/" + project_name)
     # 删除源代码中的注释
     remove_annotation()
     # 先利用solc的编译器，直接将源代码转化为抽象语法树的AST文件
