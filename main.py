5cb3160771f244552dc99f7444dd8f900491255f
eegh0407 <925791559@qq.com>
2022-07-12 16:28:39 +0800

    添加了计时的小功能

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from read_ast import read_ast
 from remove_annotation import remove_annotation
 import os
 import shutil
+import datetime
+
 if __name__ == '__main__':
+    start = datetime.datetime.now()
+    print(start)
     # 在运行之前先删除之前轮次运行出来的结果
     for project_name in os.listdir("/home/xjj/AST-GNN/data/AST_json/"):
         shutil.rmtree("/home/xjj/AST-GNN/data/AST_json/" + project_name)
@@ -13,3 +17,7 @@ if __name__ == '__main__':
     generate_ast()
     # 读取抽象语法树文件，并在其中根据json文件，生成图结构数据。
     read_ast()
+    end = datetime.datetime.now()
+    print("开始时间:", start)
+    print("结束时间:", end)
+    print("一共耗时:", end - start)
