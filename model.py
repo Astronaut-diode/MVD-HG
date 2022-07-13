15a4005e9eef1565ef66da400f9770ec4b8fcf7a
eegh0407 <925791559@qq.com>
2022-07-13 18:34:16 +0800

    后面写模型的地方

---
+++ model.py
@@ -0,0 +1,30 @@
+from torch_geometric.nn import MessagePassing, GATConv, global_mean_pool, Linear
+from typing import Optional
+from torch import Tensor
+from torch.nn import ReLU
+
+
+class ASTGNNModel(MessagePassing):
+    def __init__(self):
+        super(ASTGNNModel, self).__init__()
+        self.GATconv = GATConv(128, 1)
+        self.Linear = Linear(in_channels=1, out_channels=1)
+        self.relu = ReLU()
+
+    def forward(self, x, edge_index, batch):
+        x = self.GATconv(x, edge_index)
+        x = self.relu(x)
+        x = global_mean_pool(x=x, batch=batch)
+        x = self.Linear(x)
+        return x
+
+    def message(self, x_j: Tensor) -> Tensor:
+        pass
+
+    def aggregate(self, inputs: Tensor, index: Tensor,
+                  ptr: Optional[Tensor] = None,
+                  dim_size: Optional[int] = None) -> Tensor:
+        pass
+
+    def update(self, inputs: Tensor) -> Tensor:
+        pass
