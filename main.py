193b31c04aafe8a3c23032fb98c622fa68d33dab
eegh0407 <925791559@qq.com>
2022-07-13 15:33:59 +0800

    先判断AST_json和sol_source文件夹是否存在，不存在则直接先新建，同时添加了train函数的功能,目前还只能添加一个简陋的数据集，没有写模型

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from remove_annotation import remove_annotation
 import os
 import shutil
 import datetime
+from train import train
 
 if __name__ == '__main__':
     start = datetime.datetime.now()
     print(start)
+    # 在运行之前先判断两个data文件夹是否存在
+    if not os.path.exists("/home/xjj/AST-GNN/data/sol_source/"):
+        os.mkdir("/home/xjj/AST-GNN/data/sol_source/")
+    if not os.path.exists("/home/xjj/AST-GNN/data/AST_json/"):
+        os.mkdir("/home/xjj/AST-GNN/data/AST_json/")
     # 在运行之前先删除之前轮次运行出来的结果
     for project_name in os.listdir("/home/xjj/AST-GNN/data/AST_json/"):
         shutil.rmtree("/home/xjj/AST-GNN/data/AST_json/" + project_name)
@@ -17,6 +23,8 @@ if __name__ == '__main__':
     generate_ast()
     # 读取抽象语法树文件，并在其中根据json文件，生成图结构数据。
     read_ast()
+    # 进行训练。
+    train()
     end = datetime.datetime.now()
     print("开始时间:", start)
     print("结束时间:", end)
