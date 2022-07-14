e261b560dbedecb9d2813e784d616efbfe8dbb67
eegh0407 <925791559@qq.com>
2022-07-14 19:23:08 +0800

    修改为了直接使用config中参数的版本。

--- model.py
+++ model.py
@@ -3,13 +3,14 @@ from torch_geometric.nn import MessagePassing, GATConv, global_mean_pool, Linear
 from typing import Optional
 from torch import Tensor
 from torch.nn import ReLU
+import config
 
 
 class ASTGNNModel(MessagePassing):
     def __init__(self):
         super(ASTGNNModel, self).__init__()
         self.GATconv = GATConv(128, 1)
-        self.Linear = Linear(in_channels=1, out_channels=1)
+        self.Linear = Linear(in_channels=1, out_channels=config.classes)
         self.relu = ReLU()
 
     def forward(self, x, edge_index, batch):
