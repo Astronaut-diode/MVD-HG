0323dbf5e46263523fa86da23fb91b360a472d15
eegh0407 <925791559@qq.com>
2022-07-14 19:23:40 +0800

    修改为了直接使用config中参数的版本

--- train.py
+++ train.py
@@ -15,16 +15,16 @@ def train():
     # 获取训练用的数据集。
     dataset = ASTGNNDataset(data_path, "AST")
     # 将数据集加载到loader当中。
-    data_loader = DataLoader(dataset=dataset, batch_size=64, shuffle=True, drop_last=False)
+    data_loader = DataLoader(dataset=dataset, batch_size=config.batch_size, shuffle=True, drop_last=False)
     model = ASTGNNModel()
     # 将模型转化为在指定设备上运行的类型。
     model = model.to(device)
     # 创建优化器和反向传播函数。
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     criterion = torch.nn.CrossEntropyLoss()
     # 进行epoch个时代的训练
     model.train()
-    for epoch in range(10):
+    for epoch in range(config.epoch_size):
         train_correct = 0
         train_total = 0
         for index, batch in enumerate(data_loader):
