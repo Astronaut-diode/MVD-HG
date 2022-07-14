07c714be15c89634a9d03fde2ff1d4b822ee5cd7
eegh0407 <925791559@qq.com>
2022-07-14 21:08:42 +0800

    实现了双模型的合并计算

--- model.py
+++ model.py
@@ -4,20 +4,45 @@ from typing import Optional
 from torch import Tensor
 from torch.nn import ReLU
 import config
+import torch
 
 
 class ASTGNNModel(MessagePassing):
     def __init__(self):
         super(ASTGNNModel, self).__init__()
         self.GATconv = GATConv(128, 1)
+        self.GATconv_tmp = GATConv(128, 1)
         self.Linear = Linear(in_channels=1, out_channels=config.classes)
+        self.Linear_tmp = Linear(in_channels=1, out_channels=config.classes)
+        self.final_Linear = Linear(in_channels=2 * config.classes, out_channels=config.classes)
         self.relu = ReLU()
 
-    def forward(self, x, edge_index, batch):
+    def forward(self, ast_x, ast_edge_index, ast_batch, cfg_x, cfg_edge_index, cfg_batch):
+        # 分别计算出两个结果
+        ast_x = self.ast_forward(ast_x, ast_edge_index, ast_batch)
+        cfg_x = self.cfg_forward(cfg_x, cfg_edge_index, cfg_batch)
+        # 方式1，两个都采用0.5倍数
+        out = (ast_x + cfg_x) * 0.5
+        # 对两者的结果进行结合，然后返回结果。对他们cat以后使用全连接计算。
+        # out = torch.cat((ast_x, cfg_x), dim=1)
+        # 从双类别，合并为单类别。
+        # out = self.final_Linear(out)
+        return out
+
+    # 传入单组数据，然后计算出结果，这是抽象语法树的部分。
+    def ast_forward(self, x, edge_index, batch):
         x = self.GATconv(x, edge_index)
         x = self.relu(x)
         x = global_mean_pool(x=x, batch=batch)
         x = self.Linear(x)
+        return x
+
+    # 传入单组数据，然后计算出结果，这是控制流图的部分。
+    def cfg_forward(self, x, edge_index, batch):
+        x = self.GATconv_tmp(x, edge_index)
+        x = self.relu(x)
+        x = global_mean_pool(x=x, batch=batch)
+        x = self.Linear_tmp(x)
         return x
 
     def message(self, x_j: Tensor) -> Tensor:
--- train.py
+++ train.py
@@ -13,9 +13,11 @@ device = torch.device(config.device)
 
 def train():
     # 获取训练用的数据集。
-    dataset = ASTGNNDataset(data_path, "AST")
-    # 将数据集加载到loader当中。
-    data_loader = DataLoader(dataset=dataset, batch_size=config.batch_size, shuffle=True, drop_last=False)
+    ast_dataset = ASTGNNDataset(data_path, "AST")
+    cfg_dataset = ASTGNNDataset(data_path, "CFG")
+    # 将数据集加载到loader当中。别用shuffle，用了shuffle这两个数据集打乱的顺序就不一样了，不方便一起循环。
+    ast_data_loader = DataLoader(dataset=ast_dataset, batch_size=config.batch_size, shuffle=False, drop_last=False)
+    cfg_data_loader = DataLoader(dataset=cfg_dataset, batch_size=config.batch_size, shuffle=False, drop_last=False)
     model = ASTGNNModel()
     # 将模型转化为在指定设备上运行的类型。
     model = model.to(device)
@@ -27,18 +29,26 @@ def train():
     for epoch in range(config.epoch_size):
         train_correct = 0
         train_total = 0
-        for index, batch in enumerate(data_loader):
+        # 使用zip对他们两个进行并行循环，因为长度是一样的，所以完全可以使用。
+        for index, batch in enumerate(zip(ast_data_loader, cfg_data_loader)):
+            # 先拆出batch
+            ast_batch = batch[0]
+            cfg_batch = batch[1]
             # 将数据转化为指定设备上运行
-            batch = batch.to(device)
+            ast_batch = ast_batch.to(device)
+            cfg_batch = cfg_batch.to(device)
             # 经典的五步计算
             optimizer.zero_grad()
-            output = model(batch.x, batch.edge_index, batch.batch)
-            loss = criterion(output, batch.y)
+            # 将两个batch的节点，边还有batch信息都传进去
+            output = model(ast_batch.x, ast_batch.edge_index, ast_batch.batch, cfg_batch.x, cfg_batch.edge_index, cfg_batch.batch)
+            # 计算准确率计算一次就行了，因为两个人的结果是一样的，而且本来就是两个模型合并起来计算。
+            loss = criterion(output, ast_batch.y)
             loss.backward()
             optimizer.step()
             # 进行准确率计算。
             pred = output.argmax(dim=1)
-            train_correct = train_correct + (pred == batch.y).sum()
-            train_total = train_total + batch.num_graphs
+            print(pred)
+            train_correct = train_correct + (pred == ast_batch.y.argmax(dim=1)).sum()
+            train_total = train_total + ast_batch.num_graphs
             print("\r", epoch, index, "当前阶段的loss为{:.4f}, 正确率为{:.2f}%".format(loss, (train_correct / train_total) * 100),
                   end="")
