53f665f83c37e863b26381aaf1f529cb6dda7606
eegh0407 <925791559@qq.com>
2022-07-15 16:03:58 +0800

    之前函数写错了，如果文件夹不是空也会打印，已经删除了

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 import os
 
 
@@ -9,5 +10,8 @@ def dir_exists(dir_path):
 
 # 判断当前文件夹内容是不是空的,是空的就返回True。
 def is_blank_now_dir(dir_path):
-    print(dir_path, "由于内容已经为空，所以被删除了。")
-    return len(os.listdir(dir_path)) == 0
+    if len(os.listdir(dir_path)) == 0:
+        print(dir_path, "由于内容已经为空，所以被删除了。")
+        return True
+    else:
+        return False
