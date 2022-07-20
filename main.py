0435fd7d9e3dadf342b233fd93f9229a19eaa7aa
eegh0407 <925791559@qq.com>
2022-07-20 14:05:20 +0800

    增加了数据流的算法

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from built_vector_dataset import built_vector_dataset
 from print_tree import print_tree
 from train import train
 from gensim.models.word2vec import Word2Vec
+from append_data_flow_information import append_data_flow_information
 from tqdm import tqdm
 import datetime
 import config
@@ -49,6 +50,7 @@ if __name__ == '__main__':
                     append_method_message_by_dict(project_node_dict=project_node_dict, file_name=f"{now_dir}/{ast_json_file_name}")
                     # 传入工程文件夹完全读完以后的节点列表和节点字典，生成对应的控制流边。
                     append_control_flow_information(project_node_list=project_node_list, project_node_dict=project_node_dict, file_name=f"{now_dir}/{ast_json_file_name}")
+                    append_data_flow_information(project_node_list=project_node_list, project_node_dict = project_node_dict, file_name=f"{now_dir}/{ast_json_file_name}")
                     # 为当前这个工程文件夹中所有的文件构建语料库，如果还有下一个文件，到时候再加进去。
                     built_corpus_bfs(project_node_list=project_node_list, file_name=f"{now_dir}/{ast_json_file_name}")
                     built_corpus_dfs(project_node_list=project_node_list, file_name=f"{now_dir}/{ast_json_file_name}")
