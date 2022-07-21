a8968b7d276bf428f5209a4b8b9fd2522f1c94f2
eegh0407 <925791559@qq.com>
2022-07-21 15:47:45 +0800

    增加了关于数据流的处理方法

--- train.py
+++ train.py
@@ -13,9 +13,11 @@ def train():
     # 获取训练用的数据集。
     ast_dataset = ASTGNNDataset(config.data_dir_path, "AST")
     cfg_dataset = ASTGNNDataset(config.data_dir_path, "CFG")
-    # 将数据集加载到loader当中。别用shuffle，用了shuffle这两个数据集打乱的顺序就不一样了，不方便一起循环。
+    dfg_dataset = ASTGNNDataset(config.data_dir_path, "DFG")
+    # 将数据集加载到loader当中。别用shuffle，用了shuffle这两个数据集打乱的顺序就不一样了，不方便一起循环，不方便和zip一起使用。
     ast_data_loader = DataLoader(dataset=ast_dataset, batch_size=config.batch_size, shuffle=False, drop_last=False)
     cfg_data_loader = DataLoader(dataset=cfg_dataset, batch_size=config.batch_size, shuffle=False, drop_last=False)
+    dfg_data_loader = DataLoader(dataset=dfg_dataset, batch_size=config.batch_size, shuffle=False, drop_last=False)
     model = ASTGNNModel()
     # 将模型转化为在指定设备上运行的类型。
     model = model.to(device)
@@ -28,17 +30,19 @@ def train():
         train_correct = 0
         train_total = 0
         # 使用zip对他们两个进行并行循环，因为长度是一样的，所以完全可以使用。
-        for index, batch in enumerate(zip(ast_data_loader, cfg_data_loader)):
+        for index, batch in enumerate(zip(ast_data_loader, cfg_data_loader, dfg_data_loader)):
             # 先拆出batch
             ast_batch = batch[0]
             cfg_batch = batch[1]
+            dfg_batch = batch[2]
             # 将数据转化为指定设备上运行
             ast_batch = ast_batch.to(device)
             cfg_batch = cfg_batch.to(device)
+            dfg_batch = dfg_batch.to(device)
             # 经典的五步计算
             optimizer.zero_grad()
             # 将两个batch的节点，边还有batch信息都传进去
-            output = model(ast_batch.x, ast_batch.edge_index, ast_batch.batch, cfg_batch.x, cfg_batch.edge_index, cfg_batch.batch)
+            output = model(ast_batch.x, ast_batch.edge_index, ast_batch.batch, cfg_batch.x, cfg_batch.edge_index, cfg_batch.batch, dfg_batch.x, dfg_batch.edge_index, dfg_batch.batch)
             # 计算准确率计算一次就行了，因为两个人的结果是一样的，而且本来就是两个模型合并起来计算。
             loss = criterion(output, ast_batch.y)
             loss.backward()
