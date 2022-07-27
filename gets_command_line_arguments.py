0701c0fbd4e007b8513da6b1c1fa3ac3349b799a
eegh0407 <925791559@qq.com>
2022-07-27 16:10:31 +0800

    增加了解释

--- gets_command_line_arguments.py
+++ gets_command_line_arguments.py
@@ -4,15 +4,23 @@ import config
 
 # 获取命令行参数
 def gets_command_line_arguments():
-    parser = argparse.ArgumentParser(description='manual to this script')
+    parser = argparse.ArgumentParser(description='参数表')
     # create:创建数据集的时候用的
     # train:训练模式
     # valid:预测模式
     # truncated:语言模型受损，需要重新训练。
-    parser.add_argument('--run_mode', type=str)
-    # create_corpus_txt:仅仅创建语料库文件，当进行训练和预测的时候。
-    # generate_all: 生成所有的向量文件
-    parser.add_argument('--create_corpus_mode', type=str)
+    parser.add_argument('--run_mode', type=str,
+                        help="运行模式:"
+                             "1.create:创建数据集的时候用的。"
+                             "2.train:训练模式。"
+                             "3.valid:预测模式。"
+                             "4.truncated:语言模型受损，需要重新训练。", required=True)
+    # create_corpus_txt:仅仅创建语料库文件。
+    # generate_all: 生成所有的向量文件。
+    parser.add_argument('--create_corpus_mode', type=str,
+                        help="创建文件的模式:"
+                             "1.create_corpus_txt:仅仅创建语料库文件。"
+                             "2.generate_all: 生成所有的向量文件", required=True)
     # 下面更新config配置
     args = parser.parse_args()
     config.run_mode = args.run_mode
