34fd8ad3de2da76a0c3d7fa3f51255b75be0092d
eegh0407 <925791559@qq.com>
2022-07-27 16:12:07 +0800

    增加了回车

--- gets_command_line_arguments.py
+++ gets_command_line_arguments.py
@@ -10,17 +10,17 @@ def gets_command_line_arguments():
     # valid:预测模式
     # truncated:语言模型受损，需要重新训练。
     parser.add_argument('--run_mode', type=str,
-                        help="运行模式:"
-                             "1.create:创建数据集的时候用的。"
-                             "2.train:训练模式。"
-                             "3.valid:预测模式。"
-                             "4.truncated:语言模型受损，需要重新训练。", required=True)
+                        help="运行模式:\n"
+                             "1.create:创建数据集的时候用的。\n"
+                             "2.train:训练模式。\n"
+                             "3.valid:预测模式。\n"
+                             "4.truncated:语言模型受损，需要重新训练。\n", required=True)
     # create_corpus_txt:仅仅创建语料库文件。
     # generate_all: 生成所有的向量文件。
     parser.add_argument('--create_corpus_mode', type=str,
-                        help="创建文件的模式:"
-                             "1.create_corpus_txt:仅仅创建语料库文件。"
-                             "2.generate_all: 生成所有的向量文件", required=True)
+                        help="创建文件的模式:\n"
+                             "1.create_corpus_txt:仅仅创建语料库文件。\n"
+                             "2.generate_all: 生成所有的向量文件。\n", required=True)
     # 下面更新config配置
     args = parser.parse_args()
     config.run_mode = args.run_mode
