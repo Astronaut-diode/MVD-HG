a77d3f13b519b221767d1e70befb71bd54bb3ce4
eegh0407 <925791559@qq.com>
2022-08-02 22:11:04 +0800

    增加了疑似的分类

--- classification_of_documents.py
+++ classification_of_documents.py
@@ -13,49 +13,93 @@ def classification_of_documents():
     time = 0
     arit = 0
     dele = 0
+    suspected_reen = 0
+    suspected_time = 0
+    suspected_arit = 0
+    suspected_dele = 0
     for key in content.keys():
         # 先获取原始文件所存在的文件夹的名字
         parent_dir = os.path.basename(os.path.dirname(key))
         # 获取文件的名字
         file_name = os.path.basename(key)
-        reen += content[key][0]
         # 复制文件到重入文件夹
         if content[key][0] == 1:
+            reen += 1
             # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
             utils.dir_exists(f"{config.reentry_attack_fold}/{parent_dir}/")
             # 新文件的全路径
             target_file_path = f"{config.reentry_attack_fold}/{parent_dir}/{file_name}"
             # 复制文件到新目录中。
             shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
-        time += content[key][1]
+        # 放入到疑似重入文件夹中。
+        elif content[key][0] == 2:
+            suspected_reen += 1
+            # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
+            utils.dir_exists(f"{config.suspected_reentry_attack_fold}/{parent_dir}/")
+            # 新文件的全路径
+            target_file_path = f"{config.suspected_reentry_attack_fold}/{parent_dir}/{file_name}"
+            # 复制文件到新目录中。
+            shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
         # 复制文件到重入文件夹
         if content[key][1] == 1:
+            time += content[key][1]
             # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
             utils.dir_exists(f"{config.timestamp_attack_fold}/{parent_dir}/")
             # 新文件的全路径
             target_file_path = f"{config.timestamp_attack_fold}/{parent_dir}/{file_name}"
             # 复制文件到新目录中。
             shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
-        arit += content[key][2]
+        # 放置到疑似时间戳漏洞文件夹中
+        elif content[key][1] == 2:
+            suspected_time += 1
+            # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
+            utils.dir_exists(f"{config.suspected_timestamp_attack_fold}/{parent_dir}/")
+            # 新文件的全路径
+            target_file_path = f"{config.suspected_timestamp_attack_fold}/{parent_dir}/{file_name}"
+            # 复制文件到新目录中。
+            shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
         # 复制文件到重入文件夹
         if content[key][2] == 1:
+            arit += content[key][2]
             # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
             utils.dir_exists(f"{config.arithmetic_attack_fold}/{parent_dir}/")
             # 新文件的全路径
             target_file_path = f"{config.arithmetic_attack_fold}/{parent_dir}/{file_name}"
             # 复制文件到新目录中。
             shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
-        dele += content[key][3]
+        # 复制文件到疑似溢出文件夹
+        elif content[key][2] == 2:
+            suspected_arit += 1
+            # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
+            utils.dir_exists(f"{config.suspected_arithmetic_attack_fold}/{parent_dir}/")
+            # 新文件的全路径
+            target_file_path = f"{config.suspected_arithmetic_attack_fold}/{parent_dir}/{file_name}"
+            # 复制文件到新目录中。
+            shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
         # 复制文件到重入文件夹
         if content[key][3] == 1:
+            dele += content[key][3]
             # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
             utils.dir_exists(f"{config.dangerous_delegate_call_attack_fold}/{parent_dir}/")
             # 新文件的全路径
             target_file_path = f"{config.dangerous_delegate_call_attack_fold}/{parent_dir}/{file_name}"
             # 复制文件到新目录中。
             shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
+        # 复制文件到疑似危险调用文件夹
+        elif content[key][3] == 2:
+            suspected_dele += 1
+            # 根据文件夹名字和重入攻击的文件夹路径，可以得到新的重入攻击的全路径。
+            utils.dir_exists(f"{config.suspected_dangerous_delegate_call_attack_fold}/{parent_dir}/")
+            # 新文件的全路径
+            target_file_path = f"{config.suspected_dangerous_delegate_call_attack_fold}/{parent_dir}/{file_name}"
+            # 复制文件到新目录中。
+            shutil.copyfile(key.replace("sol_source", "complete"), target_file_path)
     print(f"重入漏洞的数量为{reen}")
+    print(f"疑似重入漏洞的数量为{suspected_reen}")
     print(f"时间戳漏洞的数量为{time}")
+    print(f"疑似时间戳漏洞的数量为{suspected_time}")
     print(f"溢出漏洞的数量为{arit}")
+    print(f"疑似溢出漏洞的数量为{suspected_arit}")
     print(f"危险调用漏洞的数量为{dele}")
-    print("文件都已经全部移入到对应的漏洞文件夹中")
+    print(f"疑似危险调用漏洞的数量为{suspected_dele}")
+    print("确认有漏洞的文件都已经全部移入到对应的漏洞文件夹中。")
