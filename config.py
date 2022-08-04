21f44588885fcff1ac9481786115071a461361cf
eegh0407 <925791559@qq.com>
2022-08-04 14:41:44 +0800

    整理了一下，顺带增加了几项新的配置，是关于度量标里面的计算参数的

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # coding=UTF-8
 import os
 
+# ========================= 运行模式 =========================
 # create:创建数据集的时候用的
 # train:训练模式
 # valid:预测模式
@@ -11,6 +12,8 @@ run_mode = "create"
 create_corpus_mode = "generate_all"
 # frozen,不删除之前的运行结果，而且运行结束的源文件会被移到success文件夹中。
 frozen = "frozen"
+# ========================= 运行模式 =========================
+# ========================= 文件夹路径 =========================
 # 记录img、data、sol_source、ast_json、complete、raw的文件夹路径
 data_dir_path = f"{os.getcwd()}/data"
 img_dir_path = f"{data_dir_path}/img"
@@ -18,7 +21,7 @@ data_sol_source_dir_path = f"{data_dir_path}/sol_source"
 data_ast_json_dir_path = f"{data_dir_path}/AST_json"
 data_complete_dir_path = f"{data_dir_path}/complete"
 data_raw_dir_path = f"{data_dir_path}/raw"
-# 保存tensorboard文件的位置
+# 保存tensor board文件的位置
 tensor_board_position = f"{data_dir_path}/tensorboard_logs"
 # 标签文件的保存地址，这是加工之前的保存位置。这时候保存的内容比较粗糙,其中保存的格式是{"0": 0, "1": 0, "2": 1}。
 idx_to_label_file = f'{data_dir_path}/idx_to_label.json'
@@ -28,7 +31,7 @@ reentry_attack_fold = f'{data_dir_path}/attack/reentry_attack_fold/'
 timestamp_attack_fold = f"{data_dir_path}/attack/timestamp_attack_fold/"
 # 溢出漏洞的文件夹
 arithmetic_attack_fold = f"{data_dir_path}/attack/arithmetic_attack/"
-# 危险调用漏洞的文件啊及
+# 危险调用漏洞的文件夹
 dangerous_delegate_call_attack_fold = f"{data_dir_path}/attack/dangerous_delegate_call_attack/"
 # 疑似重入攻击的文件夹
 suspected_reentry_attack_fold = f'{data_dir_path}/attack/suspected_reentry_attack_fold/'
@@ -36,19 +39,31 @@ suspected_reentry_attack_fold = f'{data_dir_path}/attack/suspected_reentry_attac
 suspected_timestamp_attack_fold = f"{data_dir_path}/attack/suspected_timestamp_attack_fold/"
 # 疑似溢出漏洞的文件夹
 suspected_arithmetic_attack_fold = f"{data_dir_path}/attack/suspected_arithmetic_attack/"
-# 疑似危险调用漏洞的文件啊及
+# 疑似危险调用漏洞的文件夹
 suspected_dangerous_delegate_call_attack_fold = f"{data_dir_path}/attack/suspected_dangerous_delegate_call_attack/"
 # 运行过程中出错的保存文件夹
 error_file_fold = f"{data_dir_path}/error/"
+# ========================= 文件夹路径 =========================
+# ========================= 编译配置 =========================
 # compile_files中用到的版本匹配规则,仅仅是用来判断当前行是否带有版本信息
 version_match_rule = r"pragma solidity ([><=^]* ?[\d.]{5,6} ?)+;"
 # 在机器上的编译器文件的保存位置
 compile_dir_path = "/home/xjj/.solc-select/artifacts/"
+# 版本表
+versions = ["0.4.0", "0.4.1", "0.4.2", "0.4.3", "0.4.4", "0.4.5", "0.4.6", "0.4.7", "0.4.8", "0.4.9", "0.4.10", "0.4.11", "0.4.12", "0.4.13", "0.4.14", "0.4.15", "0.4.16", "0.4.17", "0.4.18", "0.4.19", "0.4.20", "0.4.21", "0.4.22", "0.4.23", "0.4.24", "0.4.25", "0.4.26",
+            "0.5.0", "0.5.1", "0.5.2", "0.5.3", "0.5.4", "0.5.5", "0.5.6", "0.5.7", "0.5.8", "0.5.9", "0.5.10", "0.5.11", "0.5.12", "0.5.13", "0.5.14", "0.5.15", "0.5.16", "0.5.17",
+            "0.6.0", "0.6.1", "0.6.2", "0.6.3", "0.6.4", "0.6.5", "0.6.6", "0.6.7", "0.6.8", "0.6.9", "0.6.10", "0.6.11", "0.6.12",
+            "0.7.0", "0.7.1", "0.7.2", "0.7.3", "0.7.4", "0.7.5", "0.7.6",
+            "0.8.0", "0.8.1", "0.8.2", "0.8.3", "0.8.4", "0.8.5", "0.8.6", "0.8.7", "0.8.8", "0.8.9", "0.8.10", "0.8.11", "0.8.12", "0.8.13", "0.8.14", "0.8.15"]
+# ========================= 编译配置 =========================
+# ========================= 生成语言库配置 =========================
 # 词库文件的保存位置。
 corpus_file_path = f"{data_dir_path}/corpus_model.pkl"
 corpus_txt_path = f"{data_dir_path}/corpus.txt"
 # 词库文件当中，保存的每个单词的维度向量
 encode_dim = 128
+# ========================= 生成语言库配置 =========================
+# ========================= 图可视化配置 =========================
 # 是否要调用print_tree方法来显示图片
 show_plt = False
 # 是否要在print_tree方法中显示抽象语法树的边
@@ -76,6 +91,8 @@ color_dict = {"SourceUnit": "#ddd02f",
               "Break": "purple", "Continue": "purple"}
 # 忽略不显示的节点类型。
 ignore_list = ["ElementaryTypeName", "Assignment", "Literal", "VariableDeclaration", "Identifier", "UnaryOperation"]
+# ========================= 图可视化配置 =========================
+# ========================= 模型和度量标准配置 =========================
 # 最终的分类数
 classes = 4
 # 批处理数量
@@ -84,19 +101,17 @@ batch_size = 64
 learning_rate = 0.005
 # 世代数量
 epoch_size = 20
+# K折交叉验证的数量。
+k_folds = 10
 # 使用的设备
 device = "cuda:1"
-# 验证的时候使用的阈值。
-reentry_attack_valid_threshold = 0.5
-timestamp_attack_valid_threshold = 0.5
-arithmetic_attack_valid_threshold = 0.5
-delegate_attack_valid_threshold = 0.5
-# 版本表
-versions = ["0.4.0", "0.4.1", "0.4.2", "0.4.3", "0.4.4", "0.4.5", "0.4.6", "0.4.7", "0.4.8", "0.4.9", "0.4.10", "0.4.11", "0.4.12", "0.4.13", "0.4.14", "0.4.15", "0.4.16", "0.4.17", "0.4.18", "0.4.19", "0.4.20", "0.4.21", "0.4.22", "0.4.23", "0.4.24", "0.4.25", "0.4.26",
-            "0.5.0", "0.5.1", "0.5.2", "0.5.3", "0.5.4", "0.5.5", "0.5.6", "0.5.7", "0.5.8", "0.5.9", "0.5.10", "0.5.11", "0.5.12", "0.5.13", "0.5.14", "0.5.15", "0.5.16", "0.5.17",
-            "0.6.0", "0.6.1", "0.6.2", "0.6.3", "0.6.4", "0.6.5", "0.6.6", "0.6.7", "0.6.8", "0.6.9", "0.6.10", "0.6.11", "0.6.12",
-            "0.7.0", "0.7.1", "0.7.2", "0.7.3", "0.7.4", "0.7.5", "0.7.6",
-            "0.8.0", "0.8.1", "0.8.2", "0.8.3", "0.8.4", "0.8.5", "0.8.6", "0.8.7", "0.8.8", "0.8.9", "0.8.10", "0.8.11", "0.8.12", "0.8.13", "0.8.14", "0.8.15"]
+# 模型文件的保存位置
+model_data_dir = f"{data_dir_path}/model"
+# 计算度量标准的时候使用的参数
+beta = 1
+epsilon = 1e-8
+# ========================= 模型和度量标准配置 =========================
+# ========================= 漏洞建模配置 =========================
 # 构建数据流的时候最大的耐心时间，如果时间到了，就停止当前文件，因为没有必要，肯定是里面路径太多爆炸了，这里是以秒为单位的。
 create_data_flow_max_time = 40
 # 给文件进行重入攻击漏洞查询的最大时间，如果时间到了还没有找到漏洞也没有结束，那就说明是路径太多太爆炸了，提前退出，单位是秒。
@@ -105,3 +120,4 @@ make_reentry_attack_label_max_time = 40
 make_timestamp_attack_label_max_time = 40
 # 给文件进行算数溢出漏洞查询的最大时间，如果时间到了还没有找到漏洞也没有结束，那就说明是路径太多太爆炸了，提前退出，单位是秒。
 make_arithmetic_attack_label_max_time = 40
+# ========================= 漏洞建模配置 =========================
