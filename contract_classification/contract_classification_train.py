0662b83e0e067ca484b922d2bb6e8a017eaa5a44
Astronaut-diode <925791559@qq.com>
2022-12-07 09:42:34 +0800

    增加了训练集和测试集中clear和buggy的验证，一定不会出现0个的情况，但是避免不了模型自己全给出0的输出，同时给出了每次训练的详细返回值

--- contract_classification/contract_classification_train.py
+++ contract_classification/contract_classification_train.py
@@ -13,16 +13,40 @@ import utils
 
 
 def contract_classification_train():
-    # 获取完整的合约数据集，并直接分割为测试集和验证集两部分。
-    total_dataset = contract_classification_dataset(config.data_dir_path)
-    train_dataset = total_dataset[0:84]
-    test_dataset = total_dataset[84:]
+    while True:
+        # 获取完整的合约数据集，并直接分割为测试集和验证集两部分。
+        total_dataset = contract_classification_dataset(config.data_dir_path)
+        split = int(len(total_dataset) * 0.7)
+        train_dataset = total_dataset[0:split]
+        test_dataset = total_dataset[split:]
+        buggy1 = 0
+        clear1 = 0
+        tmp = DataLoader(dataset=train_dataset, batch_size=1)
+        for index, t in enumerate(tmp):
+            for i in t.contract_buggy_record:
+                if t.contract_buggy_record[i].item() == 1:
+                    buggy1 += 1
+                else:
+                    clear1 += 1
+        buggy2 = 0
+        clear2 = 0
+        tmp = DataLoader(dataset=test_dataset, batch_size=1)
+        for index, t in enumerate(tmp):
+            for i in t.contract_buggy_record:
+                if t.contract_buggy_record[i].item() == 1:
+                    buggy2 += 1
+                else:
+                    clear2 += 1
+        if buggy1 != 0 and clear1 != 0 and buggy2 != 0 and clear2 != 0:
+            print(buggy1, buggy2, clear1, clear2)
+            break
     # 获取模型。
     model = contract_classification_model()
     train_loader = DataLoader(dataset=train_dataset, batch_size=1)
     # 创建优化器和反向传播函数
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     criterion = torch.nn.BCELoss()
+    train_start_time = datetime.datetime.now()
     for epoch in range(config.epoch_size):
         # 开始训练
         model.train()
@@ -49,6 +73,8 @@ def contract_classification_train():
             count += len(train)
         print(f"{epoch + 1}.结束，一共训练了{count}张图")
         print("准确率为:", (correct / total).item(), "%总损失值为:", total_loss_of_now_epoch)
+    train_end_time = datetime.datetime.now()
+    eval_start_time = datetime.datetime.now()
     # 开始验证集部分。
     model.eval()
     test_loader = DataLoader(dataset=test_dataset, batch_size=1)
@@ -60,7 +86,20 @@ def contract_classification_train():
         predict[predict <= 0.5] = 0
         test_all_predict_labels = torch.cat((test_all_predict_labels, predict), dim=0)
         test_all_stand_labels = torch.cat((test_all_stand_labels, stand.view(-1, 1)), dim=0)
-    test_score(test_all_predict_labels, test_all_stand_labels, f"{config.attack_type_name}类型合约级结果", config.attack_type_name)
+    eval_end_time = datetime.datetime.now()
+    loader = DataLoader(dataset=total_dataset, batch_size=1)
+    total_edge_number = 0
+    total_node_number = 0
+    for data in loader:
+        total_edge_number += data.edge_index.shape[1]
+        total_node_number += data.x.shape[0]
+    utils.tip(f"总共的训练集共有{len(total_dataset)}张图，边有{total_edge_number}条，节点有{total_node_number}")
+    utils.tip(f"训练{config.epoch_size}轮一共耗时{train_end_time - train_start_time}")
+    utils.tip(f"训练图共有{split + 1}张")
+    utils.tip(f"验证一共耗时{eval_end_time - eval_start_time}")
+    utils.tip(f"验证图共有{len(total_dataset) - (split + 1)}张")
+    optimal_list = test_score(test_all_predict_labels, test_all_stand_labels, f"{config.attack_type_name}类型合约级结果", config.attack_type_name)
+    return [optimal_list[0], optimal_list[1], optimal_list[2], optimal_list[3], train_end_time - train_start_time, eval_end_time - eval_start_time, len(total_dataset), total_edge_number, total_node_number, len(train_dataset), len(test_dataset)]
 
 
 # 测试集上的度量方法，阈值已经由config中给出。
