96ad6004f0f70ba8a92180c2d234e520f3e06c39
Astronaut-diode <925791559@qq.com>
2023-03-06 08:15:19 +0800

    Node忘记上传了

--- bean/Node.py
+++ bean/Node.py
@@ -18,6 +18,14 @@ class Node:
     control_childes = []
     # 控制流的父节点
     control_parents = []
+    # 所属文件的名字
+    owner_file = ""
+    # 所属合约的名字
+    owner_contract = ""
+    # 所属函数的名字
+    owner_function = ""
+    # 所属行
+    owner_line = -1
 
     def __init__(self, node_id, node_type, parent):
         self.node_id = node_id
@@ -54,3 +62,15 @@ class Node:
     def __str__(self):
         # 这里只显示四位的原因是如果使用matplotlib画图，上面回显的字符串如果太长不是很方便显示。
         return self.node_type[0:4]
+
+    def set_owner_file(self, owner_file):
+        self.owner_file = owner_file
+
+    def set_owner_contract(self, owner_contract):
+        self.owner_contract = owner_contract
+
+    def set_owner_function(self, owner_function):
+        self.owner_function = owner_function
+
+    def set_owner_line(self, owner_line):
+        self.owner_line = owner_line
